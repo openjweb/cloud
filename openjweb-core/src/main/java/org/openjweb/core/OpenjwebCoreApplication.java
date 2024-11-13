1cb7cd0b6b2432b5901075bee59ecf83e1a94fbd
abao <29803446@qq.com>
2024-11-13 08:51:24 +0800

    调整application路径

--- openjweb-core/src/main/java/org/openjweb/core/OpenjwebCoreApplication.java
+++ openjweb-core/src/main/java/org/openjweb/core/OpenjwebCoreApplication.java
@@ -1,4 +1,4 @@
-package org.openjweb.openjwebcore;
+package org.openjweb.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

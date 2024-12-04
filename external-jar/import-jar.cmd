9c47a8c5a1b570eeb8cbb4224f1a0e7cdea03955
abao <29803446@qq.com>
2024-12-04 08:41:31 +0800

    删除不用的类

--- external-jar/import-jar.cmd
+++ external-jar/import-jar.cmd
@@ -1,1 +1,2 @@
 mvn install:install-file -Dfile=%CD%/pinyin4j-2.5.0.jar -DgroupId=net.sourceforge.pinyin4j -DartifactId=pinyin4j -Dversion=2.5.0 -Dpackaging=jar
+rem nohup java -jar  /root/openjweb-sys-0.0.1-SNAPSHOT.jar  --spring.config.additional-location=file:/root/openjweb-dev.yml &

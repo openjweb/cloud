91727f5ac0bc5e36b04fd86999f2748355fa6e20
abao <29803446@qq.com>
2024-12-04 08:50:05 +0800

    删除不用的启动类

--- openjweb-sys/src/main/java/org/openjweb/sys/job/MailJob.java
+++ openjweb-sys/src/main/java/org/openjweb/sys/job/MailJob.java
@@ -14,23 +14,24 @@ import org.springframework.stereotype.Component;
 @Configuration
 @EnableScheduling   //2.开启定时任务
 public class MailJob {
-    @Scheduled(cron = "0 0/2 * * * ?")
-    @SchedulerLock(name = "jobSendPicMail", lockAtMostFor = "60000", lockAtLeastFor = "50000")
+    //@Scheduled(cron = "0 0/2 * * * ?")
+    //@SchedulerLock(name = "jobSendPicMail", lockAtMostFor = "60000", lockAtLeastFor = "50000")
     public void doSendPicMail()   {
+        //此例在正式环境测试成功。注意须放到公网上测试
         log.info(StringUtil.getCurrentDateTime()+"开始向数据库中写入日志。。。。。。。。。。。");
         log .info("开始发带附件邮件：");
         String content="邮件正文标题";
         String smtpAddr = "smtp.163.com";
-        String sendEmail = "baozhengw@163.com";
+        String sendEmail = "xxx@163.com";
         String senderName = "众智益成";
-        String senderMailLoginId = "baozhengw@163.com";
-        String pwd =  "XPLbgGG86BzF3vLE";
+        String senderMailLoginId = "xxx@163.com";
+        String pwd =  "111111";//使用163邮箱的话，密码须申请授权码，不是登录密码
         //String pwd = FileUtil.getTextFileContent("d:\\pwd.txt", "utf-8");
         String mailTitle = "邮件标题111" ;
         //System.out.println("测试发邮件开始.........");
         try {
             Mail mail = new Mail(smtpAddr, sendEmail, senderName, senderMailLoginId,
-                    pwd, "29803446@qq.com", mailTitle, content, "465");
+                    pwd, "yyyyyy@qq.com", mailTitle, content, "465");
             mail.addAttachfile("/root/niu.png");
             log.info("开始发送邮件");
             mail.send();

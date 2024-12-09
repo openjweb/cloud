124d3c0996e34b798e6a723c7f752f2df8d88f01
abao <29803446@qq.com>
2024-12-09 11:50:12 +0800

    Beetl国际化的实现

--- openjweb-core/src/main/java/org/openjweb/core/config/I18nFunction.java
+++ openjweb-core/src/main/java/org/openjweb/core/config/I18nFunction.java
@@ -1,30 +1,28 @@
-package org.openjweb.core.config;
+    package org.openjweb.core.config;
 
+    import org.beetl.core.Context;
+    import org.beetl.core.Function;
+    import org.beetl.ext.web.WebVariable;
+    import org.springframework.web.context.WebApplicationContext;
+    import org.springframework.web.servlet.support.RequestContext;
 
+    import javax.servlet.http.HttpServletRequest;
 
-import org.beetl.core.Context;
-import org.beetl.core.Function;
-import org.beetl.ext.web.WebVariable;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.support.RequestContext;
+    public class I18nFunction implements Function {
 
-import javax.servlet.http.HttpServletRequest;
+        private WebApplicationContext wac;
 
-public class I18nFunction implements Function {
+        public I18nFunction(WebApplicationContext wac) {
+            this.wac = wac;
+        }
 
-    private WebApplicationContext wac;
+        @Override
+        public Object call(Object[] obj, Context context) {
+            HttpServletRequest request = (HttpServletRequest) context.getGlobal(WebVariable.REQUEST);
+            RequestContext requestContext = new RequestContext(request);
+            String message = requestContext.getMessage((String) obj[0]);
+            return message;
+        }
 
-    public I18nFunction(WebApplicationContext wac) {
-        this.wac = wac;
     }
-
-    @Override
-    public Object call(Object[] obj, Context context) {
-        HttpServletRequest request = (HttpServletRequest) context.getGlobal(WebVariable.REQUEST);
-        RequestContext requestContext = new RequestContext(request);
-        String message = requestContext.getMessage((String) obj[0]);
-        return message;
-    }
-
-}
 

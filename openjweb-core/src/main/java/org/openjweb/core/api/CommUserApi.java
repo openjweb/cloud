77a6ec9dc0c382c13c8430287d83c7d1cf12af09
abao <29803446@qq.com>
2024-12-22 08:31:47 +0800

    基于comm_table_def和comm_column_def的低代码开发

---
+++ openjweb-core/src/main/java/org/openjweb/core/api/CommUserApi.java
@@ -0,0 +1,23 @@
+package org.openjweb.core.api;
+
+import org.openjweb.core.service.CommUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/comm/user")
+@RestController
+public class CommUserApi {
+    @Autowired
+    CommUserService userService;
+
+    @RequestMapping("login")
+    public String login(String loginId){
+
+        return "hi";
+    }
+
+
+}

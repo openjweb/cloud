ec06a27b6023fb104dbc9ec42eae68eeb9feb3d0
abao <29803446@qq.com>
2025-08-01 19:19:25 +0800

    1

--- openjweb-sys/src/main/java/org/openjweb/sys/OpenjwebSysApplication.java
+++ openjweb-sys/src/main/java/org/openjweb/sys/OpenjwebSysApplication.java
@@ -12,7 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.openjweb"})
-@MapperScan(value = {"org.openjweb.b2c.mapper","org.openjweb.core.mapper","org.openjweb.cms.mapper"})
+@MapperScan(value = {"org.openjweb.b2c.mapper","org.openjweb.core.mapper","org.openjweb.cms.mapper,org.openjweb.pmg.mapper"})
 @EnableScheduling
 //@EnableWebSecurity
 @RequiredArgsConstructor

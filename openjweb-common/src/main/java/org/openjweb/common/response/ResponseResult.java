713e9bde62b86161eb9c749d3b9da1a47e14d0b1
abao <29803446@qq.com>
2025-08-01 19:17:44 +0800

    1

--- openjweb-common/src/main/java/org/openjweb/common/response/ResponseResult.java
+++ openjweb-common/src/main/java/org/openjweb/common/response/ResponseResult.java
@@ -1,8 +1,11 @@
 package org.openjweb.common.response;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * 通用的结果返回类
@@ -12,6 +15,16 @@ public class ResponseResult<T> implements Serializable {
 
     private Integer code;
     private String msg="操作成功";
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    private Long count;
     private T data;
 
     public ResponseResult() {
@@ -49,6 +62,39 @@ public class ResponseResult<T> implements Serializable {
         ResponseResult result = new ResponseResult();
         return result.ok(data);
     }
+    ////okResult(count, list, 0,"查询成功!");
+    public static ResponseResult okResult( Object  data, int code, String msg) {
+        ResponseResult result = new ResponseResult();
+        return result.ok(    data,  code,  msg);
+    }
+
+    public ResponseResult<?> ok( T data, int code, String msg) {
+
+        ResponseResult result = new ResponseResult();
+
+        result.code = code;
+        result.msg = msg ;
+
+        if(data instanceof IPage){
+            result.setCount( ((IPage<?>) data).getTotal());
+            result.setData( (T)((IPage)data) .getRecords());
+
+
+
+
+        }
+        else{
+            System.out.println("count is:::sssssss");
+            result.setData(data);
+
+
+        }
+
+       // this.msg = msg;
+        return result;
+    }
+
+
 
 
     public ResponseResult<?> error(Integer code, String msg) {
@@ -66,15 +112,19 @@ public class ResponseResult<T> implements Serializable {
 
     public ResponseResult<?> ok(Integer code, T data, String msg) {
         this.code = code;
-        this.data = data;
+
         this.msg = msg;
+        this.data = data;
+
         return this;
     }
+
 
     public ResponseResult<?> ok(T data) {
         this.data = data;
         return this;
     }
+
 
 
     public Integer getCode() {
